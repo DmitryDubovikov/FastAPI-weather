579ebbd7bb94dbdbfc9e14af641b811690587669
DmitryDubovikov <110840154+DmitryDubovikov@users.noreply.github.com>
2023-03-03 18:19:37 +0300

    models added

---
+++ db/models.py
@@ -0,0 +1,25 @@
+from sqlalchemy.orm import declarative_base, relationship
+from sqlalchemy.sql import func
+from sqlalchemy import Column, Integer, Float, String, DateTime, ForeignKey
+
+Base = declarative_base()
+
+
+class City(Base):
+    __tablename__ = "city"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    weathers = relationship("Weather", back_populates="city")
+
+
+class Weather(Base):
+    __tablename__ = "weather"
+
+    id = Column(Integer, primary_key=True)
+    city_id = Column(ForeignKey)
+    city = relationship("City", back_populates="weathers")
+    temperature = Column(Float)
+    pressure = Column(Integer)
+    wind = Column(Float)
+    time = Column(DateTime, server_default=func.now())
